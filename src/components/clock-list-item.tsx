75af86b7e881c887a60be7fa78ef2dec7b783090
jaewoo_byeon2 <jaewoo_byeon2@tmax.co.kr>
2022-04-13 20:43:07 +0900

    [feat] 수정 기능

--- src/components/clock-list-item.tsx
+++ src/components/clock-list-item.tsx
@@ -25,6 +25,7 @@ import { clocksAtom } from "../stores/clocks";
 import { useAtom } from "jotai";
 import { workTimeAtom } from "../stores/work-time";
 import { useState } from "react";
+import dayjs from "dayjs";
 
 const useStyles = createStyles(() => ({
   wrapper: {
@@ -57,7 +58,33 @@ const ClockListItem = ({ clock }: Props) => {
     setClocks(clocks.filter((clock) => clock.id !== id));
   };
 
-  const editClock = (id: string) => {};
+  const onChangeHour = (value: number) => {
+    setClocks(
+      clocks.map((c) => {
+        if (c.id === clock.id) {
+          return {
+            ...c,
+            date: dayjs(clock.date).hour(value).toISOString(),
+          };
+        }
+        return clock;
+      })
+    );
+  };
+
+  const onChangeMinute = (value: number) => {
+    setClocks(
+      clocks.map((c) => {
+        if (c.id === clock.id) {
+          return {
+            ...c,
+            date: dayjs(clock.date).minute(value).toISOString(),
+          };
+        }
+        return clock;
+      })
+    );
+  };
 
   return (
     <Box className={classes.wrapper}>
@@ -91,13 +118,40 @@ const ClockListItem = ({ clock }: Props) => {
             removeClock(clock.id);
           }}
         >
-          <IconCancel />
+          <IconCancel height={30} width={30} />
         </ActionIcon>
       </Group>
-      <Modal opened={modalOpened} onClose={() => setModalOpened(false)}>
+      <Modal
+        opened={modalOpened}
+        onClose={() => setModalOpened(false)}
+        title={
+          <Group spacing={0}>
+            <Text weight={700} color="blue">
+              {dayjs(clockDate).locale("ko").format("M월 D일(ddd)")}
+            </Text>
+            <Text weight={500}>의 출근 시간 수정</Text>
+          </Group>
+        }
+      >
         <Group noWrap>
-          <NumberInput label="시" />
-          <NumberInput label="분" />
+          <NumberInput
+            label="시"
+            value={dayjs(clockDate).hour()}
+            min={0}
+            max={23}
+            onChange={onChangeHour}
+            stepHoldDelay={500}
+            stepHoldInterval={100}
+          />
+          <NumberInput
+            label="분"
+            value={dayjs(clockDate).minute()}
+            min={0}
+            max={59}
+            onChange={onChangeMinute}
+            stepHoldDelay={500}
+            stepHoldInterval={100}
+          />
         </Group>
       </Modal>
     </Box>
--- src/stores/clocks.ts
+++ src/stores/clocks.ts
@@ -1,4 +1,7 @@
 import type { Clock } from "../typings/clock";
 import { atomWithStorage } from "jotai/utils";
+import { atom } from "jotai";
 
 export const clocksAtom = atomWithStorage<Clock[]>("clocks", []);
+
+export const currentClockAtom = atom<Clock | null>(null);
